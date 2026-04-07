5b8c079fb68a044d57d807096f8db4fa91ab72d2
africamonkey <marses@vip.qq.com>
2026-04-07 22:13:30 +0800

    Initial Commit

---
+++ StockDisplayTests/StockDisplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockDisplayTests.swift
+//  StockDisplayTests
+//
+//  Created by Africamonkey on 2026/4/7.
+//
+
+import Testing
+@testable import StockDisplay
+
+struct StockDisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
