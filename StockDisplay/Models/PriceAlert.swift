166cafe731a4e008a6c014b4b94f666e7c11f05e
africamonkey <marses@vip.qq.com>
2026-04-11 11:22:26 +0800

    feat: add PriceAlert model

---
+++ StockDisplay/Models/PriceAlert.swift
@@ -0,0 +1,33 @@
+import Foundation
+import SwiftData
+
+enum AlertType: String, Codable {
+    case upper
+    case lower
+}
+
+@Model
+final class PriceAlert {
+    var id: UUID
+    var stockId: UUID
+    var alertType: AlertType
+    var targetPrice: Double
+    var isEnabled: Bool
+    var hasTriggered: Bool
+
+    init(
+        id: UUID = UUID(),
+        stockId: UUID,
+        alertType: AlertType,
+        targetPrice: Double,
+        isEnabled: Bool = true,
+        hasTriggered: Bool = false
+    ) {
+        self.id = id
+        self.stockId = stockId
+        self.alertType = alertType
+        self.targetPrice = targetPrice
+        self.isEnabled = isEnabled
+        self.hasTriggered = hasTriggered
+    }
+}
